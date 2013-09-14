389a92e98179fd3f32da50f92bcad2c40b08c395
Michael Smith <mike@spokefire.co.uk>
2013-09-14 23:38:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneTest::Application.config.secret_key_base = '3277d65ea9c811e34902f1af61ffd751cd77089d771af64ccec191c9aef770b3a8383c84e7c881925114ae0cf7357667b0236aadb8cb9b98481e5860033c8b43'
