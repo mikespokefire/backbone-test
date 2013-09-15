73b5b230dbf375b4356412e447763d7d15afe50f
Michael Smith <mike@spokefire.co.uk>
2013-09-15 11:18:44 +0100

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-20.times do |i|
+print "Removing existing cards... "
+Card.destroy_all
+puts "Done"
+
+print "Seeding cards... "
+(1..20).to_a.each do |i|
   Card.create! name: "Card #{i}"
 end
+puts "Done"
